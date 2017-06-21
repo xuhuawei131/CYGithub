76805f3f1de3188c743d337551b4dba0cdcf83a7
cuiyue <380920705@qq.com>
2017-06-21 12:17:09 +0800

    增加APP类

---
+++ app/src/main/java/com/github/base/App.kt
@@ -0,0 +1,16 @@
+package com.github.base
+
+import android.app.Application
+
+/**
+ * Created by cuiyue on 2017/6/21.
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        
+
+    }
+
+}
--- app/src/main/java/com/github/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github
+package com.github.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
